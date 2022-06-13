59738f9450b13d50ed165ca2337d801812b696d8
ttomrp <ttomrp@gmail.com>
2022-06-13 02:16:23 -0500

    Models and Exceptions added.

---
+++ ServerCode/reddit/src/main/java/com/redditclone/reddit/exceptions/PostNotFoundException.java
@@ -0,0 +1,6 @@
+package com.redditclone.reddit.exceptions;
+
+public class PostNotFoundException extends RuntimeException {
+    public PostNotFoundException(String message) {
+    }
+}
---
+++ ServerCode/reddit/src/main/java/com/redditclone/reddit/exceptions/SpringRedditException.java
@@ -0,0 +1,11 @@
+package com.redditclone.reddit.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage, Exception exception) {
+        super(exMessage, exception);
+    }
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
