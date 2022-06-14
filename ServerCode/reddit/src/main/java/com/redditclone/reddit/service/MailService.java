2f85cbf7a87acc671f2e5d721326a65475f6d19a
ttomrp <ttomrp@gmail.com>
2022-06-14 15:39:50 -0500

    email verification logic added

---
+++ ServerCode/reddit/src/main/java/com/redditclone/reddit/service/MailService.java
@@ -0,0 +1,42 @@
+package com.redditclone.reddit.service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.redditclone.reddit.exceptions.SpringRedditException;
+import com.redditclone.reddit.model.NotificationEmail;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j // 4J logger
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("redditclone@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(notificationEmail.getBody());
+        };
+
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Activation email sent!");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending email", e);
+            throw new SpringRedditException(
+                    "Exception occurred when sending email to " + notificationEmail.getRecipient(), e);
+        }
+    }
+
+}
